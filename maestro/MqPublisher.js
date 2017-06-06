9ea204ad5efbd4921ab08173d89ea378636882bb
bikov <bikov95@gmail.com>
2017-06-06 09:19:19 +0300

    infinity while to send messages

--- maestro/MqPublisher.js
+++ maestro/MqPublisher.js
@@ -2,27 +2,27 @@
  * Created by bikov on 6/4/2017.
  */
 
-let amqp = require('amqplib/callback_api');
+let amqp = require('amqplib/callback_api'),
+    Promise = require('bluebird'),
+    promiseWhile = require('promise-while')(Promise);
 
-function sandMessages(ch, conn) {
-    ch.assertQueue('', {exclusive: true}, function (err, q) {
-        if (err) throw err;
-        let corr = generateUuid();
-        console.log(`sending message with uuid: ${corr}`);
+function sendMessages(ch) {
+    return new Promise(function (resolve) {
+        ch.assertQueue('', {exclusive: true}, function (err, q) {
+            if (err) throw err;
+            let corr = generateUuid();
+            console.log(`sending message with uuid: ${corr}`);
 
-        ch.consume(q.queue, function (msg) {
-            if (msg.properties.correlationId == corr) {
-                console.log(' [.] Got %s', msg.content.toString());
-                setTimeout(function () {
-                    console.log('closing');
-                    conn.close();
-                    process.exit(0)
-                }, 5);
-            }
-        }, {noAck: true});
-        let message;
-        Math.random() >= 0.8 ? message = 'blat' : message = '1234';
-        ch.sendToQueue('rpc_queue', new Buffer(message), {correlationId: corr, replyTo: q.queue});
+            ch.consume(q.queue, function (msg) {
+                if (msg.properties.correlationId == corr) {
+                    console.log(' [.] Got %s', msg.content.toString());
+                    resolve();
+                }
+            }, {noAck: true});
+            let message;
+            Math.random() >= 0.8 ? message = 'blat' : message = '1234';
+            ch.sendToQueue('rpc_queue', new Buffer(message), {correlationId: corr, replyTo: q.queue});
+        })
     });
 }
 function send() {
@@ -30,7 +30,14 @@ function send() {
         if(err) throw err;
         conn.createChannel(function(err, ch) {
             if(err) throw err;
-            sandMessages(ch, conn);
+            promiseWhile(
+                function() {
+                    return true; // infinite loop
+                },
+                function() {
+                    return sendMessages(ch);
+                }
+            );
         });
     });
 }
