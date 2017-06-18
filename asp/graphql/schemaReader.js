815621037a5c9d6c49f5db0c573358be23955cf1
bikov <bikov95@gmail.com>
2017-06-18 09:41:31 +0300

    change schemaReader

--- asp/graphql/schemaReader.js
+++ asp/graphql/schemaReader.js
@@ -9,30 +9,30 @@ function readSchema(host, entryPoint) {
 }
 
 
-function sendIntro(introQuery, host, entryPoint) {
-    return new Promise(function (resolve, reject) {
-        const post_options = {
-            host: host,
-            port: '80',
-            path: entryPoint,
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': Buffer.byteLength(introQuery)
-            }
-        };
-
-        let post_req = http.request(post_options, function (res) {
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
-                resolve(chunk);
-            });
-        });
-
-        post_req.write(introQuery);
-        post_req.end();
-    })
-}
+// function sendIntro(introQuery, host, entryPoint) {
+//     return new Promise(function (resolve, reject) {
+//         const post_options = {
+//             host: host,
+//             port: '80',
+//             path: entryPoint,
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded',
+//                 'Content-Length': Buffer.byteLength(introQuery)
+//             }
+//         };
+//
+//         let post_req = http.request(post_options, function (res) {
+//             res.setEncoding('utf8');
+//             res.on('data', function (chunk) {
+//                 resolve(chunk);
+//             });
+//         });
+//
+//         post_req.write(introQuery);
+//         post_req.end();
+//     })
+// }
 
 module.exports = {
     "readSchema": readSchema
