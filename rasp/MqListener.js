d4a43b9288d37d4e696bc28881353b5aa9426f63
bikov <bikov95@gmail.com>
2017-06-18 14:57:01 +0300

    MqListener now takes queries and validates them

--- rasp/MqListener.js
+++ rasp/MqListener.js
@@ -1,10 +1,12 @@
 let amqp = require('amqplib/callback_api');
 let queryValidator = require('./graphql/validator/requestValidator'),
-    winston = require('winston');
+    redis = require("../godfather/redis/redis");
+winston = require('winston');
 
-function listen(schema) {
-    amqp.connect(process.env.MQ_URL || 'amqp://bikov:blat@localhost', function(err, conn) {
-        if(err) return reconnectToMq(err);
+
+function listen() {
+    amqp.connect(process.env.MQ_URL || 'amqp://bikov:blat@localhost', function (err, conn) {
+        if (err) return reconnectToMq(err);
         conn.on('close', function (reason) {
             return reconnectToMq(reason);
         });
@@ -14,19 +16,24 @@ function listen(schema) {
             ch.assertQueue(q, {durable: false});
             ch.prefetch(1);
             winston.info('Listening to work');
-            ch.consume(q, function reply(msg) {
-                let validationResponse = queryValidator.validateQuery(schema, query);
-                let timeOut = 0;
-                setTimeout(()=> {
-                    ch.sendToQueue(query.properties.replyTo,
-                        new Buffer(validationResponse.toString()),
-                        {correlationId: query.properties.correlationId});
-                    ch.ack(query);
-                }, timeOut);
+            ch.consume(q, (msg) => {
+                validate(msg.content)
+                    .then((validateResponse) =>{
+                        ch.sendToQueue(msg.properties.replyTo,
+                            new Buffer(validateResponse.toString()),
+                            {correlationId: msg.properties.correlationId});
+                        ch.ack(msg);
+                    })
             });
         });
     });
 }
+
+function validate(request) {
+    return redis.getGql(request['version'])
+        .then((schema) => queryValidator.validateQuery(schema, request["query"]));
+}
+
 function reconnectToMq(reason) {
     winston.warn(`Lost connection to RMQ because:${reason}.  Reconnecting in 3 seconds...`);
     return (setTimeout(listen, 3000));
