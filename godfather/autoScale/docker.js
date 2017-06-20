ecef0d567562a2e2ea32dc2983669debd8579c46
bikov <bikov95@gmail.com>
2017-06-20 13:41:43 +0300

    container restart now written kill and then create new and start, need to check on linux

---
+++ godfather/autoScale/docker.js
@@ -0,0 +1,55 @@
+/**
+ * Created by bikov on 6/18/2017.
+ */
+let Docker = require('dockerode'),
+    winston = require('winston');
+
+function restartDocker(id, cb = ()=>{}) {
+    return new Promise((resolve,reject)=> {
+        winston.info(`restarting container with id  ${id}`);
+        let docker = new Docker(),
+            container = docker.getContainer(id);
+
+        container.kill({t:0})
+            .then(function (container) {
+                return container.remove()
+            }).then(function (data) {
+            winston.info(`container by id ${id} killed and removed`)
+        }).then(() => createRaspContainer(docker))
+            .then((container) => container.start())
+            .then((container) => winston.info(`container with id ${container.id} started`))
+            .then(resolve)
+            .catch((err) => {
+                winston.error(`Unable to restart container with id ${container.id} because ${err}`);
+                reject(err);
+            })
+        });
+    //
+    //
+    //
+    //     winston.info(`restarting container with id ${id} !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
+    //     container.restart({t: 0}, (err, data)=>{
+    //         if(err) {
+    //             reject(err);
+    //             return cb(err);
+    //         }
+    //         winston.info(`container with id ${id} restarted, data: ${data}`);
+    //         resolve(data);
+    //         cb(null, data);
+    //     })
+    // });
+}
+
+function createRaspContainer(docker) {
+    return docker.createContainer({
+        Image: 'bikov/rasp',
+        HostConfig:{
+            Links: ["rabbit:mq","redis:redis"]
+        }
+
+    })
+}
+
+module.exports = {
+    "restartDocker": restartDocker
+};
